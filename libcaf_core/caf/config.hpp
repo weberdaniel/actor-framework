264ffcbf266c8bf872dfb52ef024edbaa2edb857
weberdaniel <48121307+weberdaniel@users.noreply.github.com>
2022-07-05 01:06:08 +0200

    try libunwind...

--- libcaf_core/caf/config.hpp
+++ libcaf_core/caf/config.hpp
@@ -213,6 +213,7 @@ struct IUnknown;
 
 #include <cstdio>
 #include <cstdlib>
+#include <libunwind.h>
 
 // Optionally enable CAF_ASSERT
 #ifndef CAF_ENABLE_RUNTIME_CHECKS
@@ -226,7 +227,6 @@ struct IUnknown;
     static_cast<void>(0)
 #else // defined(CAF_LINUX) || defined(CAF_MACOS)
 #  define UNW_LOCAL_ONLY
-#  include <libunwind.h>
 #  define CAF_ASSERT(stmt)                                                     \
     if (static_cast<bool>(stmt) == false) {                                    \
       printf("%s:%u: requirement failed '%s'\n", __FILE__, __LINE__, #stmt);   \
